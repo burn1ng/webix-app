e9fa109850d460da91c484a9930726022bfb8acf
burn1ng <andrew.burn1ng@gmail.com>
2017-07-26 13:53:15 +0300

    standardJS codeStyle inregrated

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
 /*
-	App configuration
+App configuration
 */
-
 define([
-	"libs/webix-jet-core/core",
-	"libs/webix-jet-core/plugins/menu",
-	"libs/webix-jet-core/plugins/theme",
-	"libs/webix-jet-core/plugins/locale"
-], function(
-	core, menu, theme, locale
-){
-
-	//configuration
-	var app = core.create({
-		id:         "app-core-id",
-		name:       "English App!",
-		version:    "0.1.0",
-		debug:      true,
-		start:      "/sign-in"
-	});
-
-	app.use(menu);
-
-	app.use(theme);
-	app.use(locale);
-	return app;
-});
+  'libs/webix-jet-core/core',
+  'libs/webix-jet-core/plugins/menu',
+  'libs/webix-jet-core/plugins/theme',
+  'libs/webix-jet-core/plugins/locale'
+], function (core, menu, theme, locale) {
+  // configuration
+  var app = core.create({
+    id: 'app-core-id',
+    name: 'English App!',
+    version: '0.1.0',
+    debug: true,
+    start: '/sign-in'
+  })
+  app.use(menu)
+  app.use(theme)
+  app.use(locale)
+  return app
+})
