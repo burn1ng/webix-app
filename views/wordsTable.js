3738e5ee0800bad2228bd155256cf28ac1c365fc
Andrei Dushkov <andrew.burn1ng@gmail.com>
2017-08-15 22:30:53 +0300

    huge changes in dataTable view

--- views/wordsTable.js
+++ views/wordsTable.js
@@ -1,12 +1,13 @@
 define([
-    // 'models/words'
+    'models/words'
 ], (words) => {
     function addRow() {
-        $$('gridDatatable').add({originalWord: 'New word', translationWord: '', partOfSpeech: 0});
+        words.arrayOfWords.add({originalWord: 'New word', translationWord: '', partOfSpeech: 0});
     }
     function deleteRow() {
-        $$('gridDatatable').remove($$('gridDatatable').getSelectedId(true));
+        words.arrayOfWords.remove($$('gridDatatable').getSelectedId(true));
     }
+
     function showDataStatus(grid) {
         if (!grid.count()) {
             grid.showOverlay('There\'s no data');
@@ -23,47 +24,17 @@ define([
         }
     }, webix.ui.datafilter.summColumn);
 
-    // custom proxy for datatable editing in row, and upload ALL changes in 1 call to server
-    webix.proxy.restWithDataProcessorDelay = {
-        $proxy: true,
-        load(view, callback) {
-            webix.ajax(this.source, callback, view);
-        },
-        save(view, update, dp, callback) {
-            return webix.delay(() => {
-                webix.proxy.restWithDataProcessorDelay._save_logic.call(this, view, update, dp, callback, webix.ajax());
-            });
-        },
-
-        _save_logic(view, update, dp, callback, ajax) {
-            let url = this.source;
-            let query = '';
-            let mark = url.indexOf('?');
-
-            if (mark !== -1) {
-                query = url.substr(mark);
-                url = url.substr(0, mark);
-            }
-
-            url += url.charAt(url.length - 1) == '/' ? '' : '/';
-            let mode = update.operation;
-
-
-            let data = update.data;
-            if (mode === 'insert') delete data.id;
-
-            // call restWithDataProcessorDelay URI
-            if (mode === 'update') {
-                ajax.put(url + data.id + query, data, callback);
-            }
-            else if (mode === 'delete') {
-                ajax.del(url + data.id + query, data, callback);
-            }
-            else {
-                ajax.post(url + query, data, callback);
-            }
+    // custom textFilter logic for render icon in the right corner of input
+    webix.ui.datafilter.customTextFilter = webix.extend({
+        render(master, config) {
+            if (this.init) this.init(config);
+            config.css = 'webix_ss_filter';
+            // render input and icon
+            return `<input ${config.placeholder ?
+                `placeholder="${config.placeholder}" ` : ''}type='text'>` +
+            ' type=\'text\'><span class=\'webix_icon fa-search\'></span></input>';
         }
-    };
+    }, webix.ui.datafilter.textFilter);
 
     let toolbar = {
         id: 'toolbarForDatatable',
@@ -75,53 +46,13 @@ define([
         ]
     };
 
-    let testToolbar = {
-        view: 'toolbar',
-        elements: [
-            {view: 'search', width: 300},
-            {},
-            {view: 'button', type: 'iconButton', icon: 'sign-in'}
-        ]};
-
-
-    let exportToolbar = {
-        id: 'exportToolbar',
-        view: 'toolbar',
-        elements: [
-            {
-                view: 'button',
-                type: 'iconButton',
-                icon: 'sign-in',
-                value: 'Export'
-            },
-            {
-                view: 'button',
-                value: 'Export to Excel',
-                click() {
-                    webix.toExcel($$('gridDatatable'), {
-                        filename: 'myVocabulary',
-                        name: 'English words'
-                    });
-                }
-            },
-            {
-                gravity: 1
-            },
-            {
-                view: 'button',
-                value: 'Generate test!',
-                align: 'right'
-            }
-        ]
-    };
-
     let grid = {
         id: 'gridDatatable',
-        url: 'api/getWords', // simple load data
-        save: {
-            url: 'restWithDataProcessorDelay->/api/word', // custom proxy
-            updateFromResponse: true
-        },
+        // url: 'api/getWords', // simple load data
+        // save: {
+        //     url: 'restWithDataProcessorDelay->/api/word', // custom proxy
+        //     updateFromResponse: true
+        // },
         view: 'datatable',
         editable: true,
         resizeColumn: true,
@@ -134,18 +65,6 @@ define([
         on: {
             onItemDblClick(id) {
                 this.editRow(id);
-            },
-            onBeforeLoad() {
-                this.showOverlay('Loading...');
-            },
-            onAfterLoad() {
-                showDataStatus(this);
-            },
-            onAfterDelete() {
-                showDataStatus(this);
-            },
-            onAfterAdd() {
-                showDataStatus(this);
             },
             'data->onStoreUpdated': function () {
                 this.data.each((obj, i) => {
@@ -164,7 +83,7 @@ define([
                 id: 'originalWord',
                 fillspace: 2,
                 editor: 'text',
-                header: ['Original word', {content: 'textFilter', placeholder: 'Find word'}],
+                header: ['Original word', {content: 'customTextFilter', placeholder: 'Find word'}],
                 sort: 'string',
                 footer: {text: 'Total', colspan: 3}
             },
@@ -172,7 +91,7 @@ define([
                 id: 'translationWord',
                 fillspace: 4,
                 editor: 'text',
-                header: ['Translation', {content: 'textFilter', placeholder: 'Find translation'}],
+                header: ['Translation', {content: 'customTextFilter', placeholder: 'Find translation'}],
                 sort: 'string'
             },
             {
@@ -195,13 +114,81 @@ define([
         ]
     };
 
+    let dtFooterToolbar = {
+        id: 'exportToolbar',
+        view: 'toolbar',
+        elements: [
+            {
+                view: 'button',
+                type: 'iconButton',
+                icon: 'file-image-o',
+                label: 'Export to Png',
+                adjust: true,
+                click() {
+                    // TODO
+                }
+            },
+            {},
+            {
+                view: 'icon',
+                tooltip: 'Export to Png',
+                icon: 'file-image-o',
+                click() {
+                    webix.toPNG($$('gridDatatable'), {
+                        filename: 'myVocabulary'
+                    });
+                }
+            },
+            {
+                view: 'icon',
+                tooltip: 'Export to Pdf',
+                icon: 'file-pdf-o',
+                click() {
+                    webix.toPDF($$('gridDatatable'), {
+                        filename: 'myVocabulary'
+                    });
+                }
+            },
+            {
+                view: 'icon',
+                tooltip: 'Export to Excel',
+                icon: 'file-excel-o',
+                label: 'Export to Excel',
+                click() {
+                    webix.toExcel($$('gridDatatable'), {
+                        filename: 'myVocabulary',
+                        name: 'English words'
+                    });
+                }
+            },
+            {},
+            {
+                view: 'button',
+                type: 'iconButton',
+                icon: 'cog',
+                label: 'Generate test!',
+                click() {
+                    // TODO
+                }
+            }
+        ]
+    };
+
+    let testForm = {
+        view: 'form',
+        elements: [
+            {view: 'text', label: 'Group Name', placeholder: 'New group'},
+            {cols: [
+                {view: 'button', value: 'Login', type: 'form'}
+            ]}
+        ]};
 
     let ui = {
         rows: [
             toolbar,
             grid,
-            exportToolbar,
-            testToolbar
+            dtFooterToolbar,
+            testForm
         ]
     };
 
@@ -216,7 +203,21 @@ define([
             // $scope.on(words.arrayOfWords, 'onDataUpdate', () => {
             //     popup.show();
             // });
-            // $$('gridDatatable').parse(words.arrayOfWords);
+
+
+            $$('gridDatatable').data.sync(words.arrayOfWords);
+            words.arrayOfWords.attachEvent('onBeforeLoad', () => {
+                $$('gridDatatable').showOverlay('Loading...');
+            });
+            words.arrayOfWords.attachEvent('onAfterLoad', () => {
+                showDataStatus($$('gridDatatable'));
+            });
+            words.arrayOfWords.attachEvent('onAfterDelete', () => {
+                showDataStatus($$('gridDatatable'));
+            });
+            words.arrayOfWords.attachEvent('onAfterAdd', () => {
+                showDataStatus($$('gridDatatable'));
+            });
         }
     };
 });
