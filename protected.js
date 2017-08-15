d7846d432af4a9d130c15d1e2c99601ed5061590
Andrei Dushkov <andrew.burn1ng@gmail.com>
2017-08-15 17:58:09 +0300

    change onBeforeAjax logic

--- protected.js
+++ protected.js
@@ -13,13 +13,17 @@ define([
         name: 'English App!',
         version: '0.1.0',
         debug: true,
-        start: '/top/start'
+        start: '/top/dashboard'
     });
+
+    let urlWithoutHeaders = '//cdn.webix.com/extras/xlsx.core.min.js';
 
     webix.attachEvent('onBeforeAjax', (mode, url, data, request, headers) => {
         console.log('onBeforeAjax event fired: \n');
-        console.log(data);
-        headers.Authorization = localStorage.getItem('token');
+        // we need to check url, because webix CDN don't allow Authorization token
+        if (url.substring(0, urlWithoutHeaders.length) !== urlWithoutHeaders) {
+            headers.Authorization = localStorage.getItem('token');
+        }
     });
 
     webix.attachEvent('onAjaxError', (requestObj) => {
