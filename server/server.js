594b49aa0f9bfeffd949ac79f09620f8caba07d3
Andrei Dushkov <andrew.burn1ng@gmail.com>
2017-08-22 11:25:19 +0300

    move mongoose connections before starting server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
-const Koa = require('koa'); // ядро
-const Router = require('koa-router'); // маршрутизация
-const bodyParser = require('koa-bodyparser'); // парсер для POST запросов
-const serve = require('koa-static'); // модуль, который отдает статические файлы типа index.html из заданной директории
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser'); // parser for POST requests
+const serve = require('koa-static'); // send static files such as index.html from current directory
 const logger = require('koa-logger'); // опциональный модуль для логов сетевых запросов. Полезен при разработке.
-const passport = require('koa-passport'); // реализация passport для Koa
-const mongoose = require('mongoose'); // стандартная прослойка для работы с MongoDB
+const passport = require('koa-passport');
+const mongoose = require('mongoose'); // object model for MongoDB
 const db = require('./config/db');
 const appRoutes = require('./routes/');
 const localStrategy = require('./passport/local');
 const jwtStrategy = require('./passport/jwt');
+
+mongoose.Promise = Promise; // tell Mongoose to use standard Promises
+mongoose.set('debug', true); // tell Mongoose to write all request to db in console. convinient for debugging
+mongoose.connect(db.url, {useMongoClient: true});
+
+mongoose.connection.on('connected', () => {
+    console.log(`\n mongoose connected to ${db.url} \n`);
+});
+mongoose.connection.on('disconnected', () => {
+    console.log('mongoose disconnected');
+});
+mongoose.connection.on('error', console.error.bind(console, 'connection error: '));
+
 
 const app = new Koa();
 const router = new Router();
@@ -27,21 +40,3 @@ app.listen(3000);
 passport.use(localStrategy);
 passport.use(jwtStrategy);
 
-mongoose.Promise = Promise; // Просим Mongoose использовать стандартные Промисы
-mongoose.set('debug', true); // Просим Mongoose писать все запросы к базе в консоль. Удобно для отладки кода
-mongoose.connect(db.url, {useMongoClient: true});
-
-mongoose.connection.on('error', (err) => {
-    if (err) throw err;
-});
-mongoose.connection.on('connected', () => {
-    console.log(`\n mongoose connected to ${db.url} \n`);
-});
-mongoose.connection.on('disconnected', () => {
-    console.log('mongoose disconnected');
-});
-mongoose.connection.on('disconnected', () => {
-    console.log('mongoose disconnected');
-});
-
-mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
