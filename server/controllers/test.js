e367df5955e808e126b289a1cf726e91259a0707
Andrei Dushkou <andrew.burn1ng@gmail.com>
2017-08-27 13:17:03 +0300

    generating randomData for test implemented (server-side )

--- server/controllers/test.js
+++ server/controllers/test.js
@@ -6,9 +6,7 @@ module.exports = {
     // CREATE
     async generateTest(ctx) {
         let wordGroupId = ctx.request.body.wordGroupId;
-        console.log(`_wordGroup in req: ${wordGroupId}`);
         let countOfWords = ctx.request.body.count;
-        console.log(`countOfWords in req: ${countOfWords}`);
 
         if (!wordGroupId && !countOfWords) {
             ctx.status = 400;
@@ -28,51 +26,23 @@ module.exports = {
         catch (err) {
             ctx.throw(500, 'Problem with creating test record in db', {err});
         }
-
         // get Random data for test
         const filter = {_wordGroup: wordGroupId};
         const fields = {originalWord: 1, translationWord: 1, partOfSpeech: 1};
+        let options = {limit: countOfWords > 10 ? 10 : countOfWords};
 
-        if (countOfWords > 0 && countOfWords <= 10) {
-            let options = {limit: countOfWords};
-
-            // Word.findRandom(filter, fields, options, (err, randomWords) => {
-            //     if (!err) {
-            //         console.log(randomWords);
-            //         ctx.body = randomWords;
-            //     }
-            //     else {
-            //         ctx.throw(500, 'Problem with generating data when countOfWords > 0 && <= 10', {err});
-            //     }
-            // });
-
-            // ctx.body = 'ok';
-
-            let promise = new Promise((resolve, reject) => {
-                Word.findRandom(filter, fields, options, (err, randomWords) => {
-                    err ? reject(err) : resolve(randomWords);
-                });
+        let promiseDecorator = new Promise((resolve, reject) => {
+            Word.findRandom(filter, fields, options, (err, randomWords) => {
+                if (err) { reject(err); }
+                else { resolve(randomWords); }
             });
+        });
 
-            promise.then(
-                (randomWords) => {
-                    console.log(randomWords);
-                    ctx.body = randomWords;
-                },
-                (err) => {
-                    ctx.throw(500, 'Problem with generating data when countOfWords > 0 && <= 10', {err});
-                }
-            );
-        }
-        else if (countOfWords > 10) {
-            try {
-                let options = {limit: 10};
-                let randomWords = await Word.findRandom(filter, fields, options);
-                ctx.body = randomWords;
-            }
-            catch (err) {
-                ctx.throw(500, 'Problem with generating data when countOfWords > 10', {err});
-            }
-        }
+        await promiseDecorator.then((randomWords) => {
+            ctx.body = randomWords;
+        }).catch((err) => {
+            // TODO: remove current TEST from db
+            ctx.throw(500, 'Problem with generating random data', {err});
+        });
     }
 };
