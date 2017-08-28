f6a874844fbf75a66c41f85b1ba575e0b1e6dea4
Andrei Dushkov <andrew.burn1ng@gmail.com>
2017-08-28 19:15:39 +0300

    implemet correct data generation for 2 test (eng->rus, rus->eng)

--- server/controllers/test.js
+++ server/controllers/test.js
@@ -26,40 +26,73 @@ module.exports = {
         catch (err) {
             ctx.throw(500, 'Problem with creating test record in db', {err});
         }
+
         // get Random data for test
-        let findRandomWords = new Promise((resolve, reject) => {
-            let filter = {_wordGroup: wordGroupId};
-            let fields = {originalWord: 1, translationWord: 1, partOfSpeech: 1};
-            let options = {limit: countOfWords > 10 ? 10 : countOfWords};
-            Word.findRandom(filter, fields, options, (err, randomWords) => {
-                if (err) { reject(err); }
-                else { resolve(randomWords); }
+        function shuffle(a) {
+            for (let i = a.length; i; i--) {
+                let j = Math.floor(Math.random() * i);
+                [a[i - 1], a[j]] = [a[j], a[i - 1]];
+            }
+        }
+        try {
+            let jsonResponse = [];
+
+            let typeOfTest = +ctx.request.body.typeOfTest; // convert to number
+
+            let findRandomQuestions = await new Promise((resolve, reject) => {
+                let filter = {_wordGroup: wordGroupId};
+                let fields = {originalWord: 1, translationWord: 1, partOfSpeech: 1};
+                let options = {limit: countOfWords > 10 ? 10 : countOfWords};
+                Word.findRandom(filter, fields, options, (err, randomQuestions) => {
+                    if (err) { reject(err); }
+                    else { resolve(randomQuestions); }
+                });
             });
-        });
 
-        // let findRandomVariants = new Promise((resolve, reject) => {
-        //     let filter = {_wordGroup: wordGroupId};
-        // });
+            await Promise.all(findRandomQuestions.map(async (randomQuestion, i) => {
+                let quest = typeOfTest === 0 ?
+                    randomQuestion.originalWord : randomQuestion.translationWord;
+                let ans = typeOfTest === 0 ?
+                    randomQuestion.translationWord : randomQuestion.originalWord;
 
-        await findRandomWords.then((randomWords) => {
-            for (let i = 0; i < randomWords.length; i++) {
-                let findRandomVariants = new Promise((resolve, reject) => {
-                    let filter = {partOfSpeech: randomWords[i].partOfSpeech};
-                    let fields = {originalWord: 1, translationWord: 1, partOfSpeech: 1};
+                jsonResponse[i] = {
+                    question: quest,
+                    correctAnswer: ans
+                };
+
+                let findRandomVariants = await new Promise((resolve, reject) => {
+                    let filter = {
+                        partOfSpeech: randomQuestion.partOfSpeech,
+                        originalWord: {$ne: randomQuestion.originalWord} // or translation $ne randquest.translation
+                    };
+                    let fields = {[typeOfTest === 0 ? 'translationWord' : 'originalWord']: 1};
                     let options = {limit: 3};
                     Word.findRandom(filter, fields, options, (err, randomVariants) => {
                         if (err) { reject(err); }
                         else { resolve(randomVariants); }
                     });
                 });
-            }
 
-            // ctx.body = randomWords;
-        });
 
-        findRandomWords.catch((err) => {
-            // TODO: remove current TEST from db
+                let middleArr = findRandomVariants.map((randomVariant) => {
+                    let variantValue = typeOfTest === 0 ? randomVariant.translationWord :
+                        randomVariant.originalWord;
+
+                    return variantValue;
+                });
+
+                middleArr.push(typeOfTest === 0 ? randomQuestion.translationWord :
+                    randomQuestion.originalWord);
+
+                shuffle(middleArr);
+
+                jsonResponse[i].variants = middleArr;
+            }));
+
+            ctx.body = jsonResponse;
+        }
+        catch (err) {
             ctx.throw(500, 'Problem with generating random data', {err});
-        });
+        }
     }
 };
--- views/grid/gridBottomToolbar.js
+++ views/grid/gridBottomToolbar.js
@@ -62,10 +62,18 @@ define([
             {},
             {
                 view: 'button',
-                id: 'gridBottomToolbar:testGenerate',
+                id: 'en_rus_test',
                 type: 'iconButton',
                 icon: 'cog',
-                label: _('test_generate'),
+                label: 'Test English words!', // _('test_generate'),
+                autowidth: true
+            },
+            {
+                view: 'button',
+                id: 'rus_en_test',
+                type: 'iconButton',
+                icon: 'cog',
+                label: 'Test Russian words!', // _('test_generate'),
                 autowidth: true
             }
         ]
@@ -76,35 +84,52 @@ define([
         $oninit: (view, $scope) => {
             webix.extend($$('gridDatatable'), webix.ProgressBar);
 
-            $$('gridBottomToolbar:testGenerate').attachEvent('onItemClick', () => {
+            let done;
+
+            function generateTestHandler(id) {
+                // $$(id).attachEvent('onItemClick', () => {
                 let selectedWordGroup = $$('wordGroupList:dataview').getSelectedItem();
                 if (selectedWordGroup) {
                     let currentCount = selectedWordGroup.count;
 
                     if (!currentCount) {
                         webix.message({type: 'warning', text: 'Please, add words in your wordgroup before test generating!'});
+                        return;
                     }
 
-                    $$('gridDatatable').showProgress({type: 'icon', delay: 3000});
-                    webix.message({text: `Test for ${selectedWordGroup.wordGroupName} is genearting now...`});
+                    // prevent several clicks
+                    (() => {
+                        if (!done) {
+                            done = true;
 
-                    let promise = webix.ajax().post('/api/generateTest', {
-                        wordGroupId: selectedWordGroup._id,
-                        count: currentCount
-                    });
+                            $$('gridDatatable').showProgress({type: 'icon', delay: 3000});
+                            webix.message({text: `Test for ${selectedWordGroup.wordGroupName} is genearting now...`});
 
-                    promise.then((randomWords) => {
-                        console.log(randomWords.json());
-                        dataForTest.randomData = randomWords.json();
-                        app.show('test');
-                    }).fail((err) => {
-                        webix.message({type: 'warning', text: `Sorry, problems with test generating: ${err}`});
-                    });
+                            let promise = webix.ajax().post('/api/generateTest', {
+                                wordGroupId: selectedWordGroup._id,
+                                count: currentCount,
+                                typeOfTest: id === 'en_rus_test' ? 0 : 1 // 0 - eng to rus test, 1 - rus to eng test
+                            });
+
+                            promise.then((randomData) => {
+                                console.log({typeOfTest: id === 'en_rus_test' ? 0 : 1});
+                                console.log(randomData.json());
+                                dataForTest.data = randomData.json();
+                                app.show('test');
+                            }).fail((err) => {
+                                webix.message({type: 'warning', text: `Sorry, problems with test generating: ${err}`});
+                            });
+                        }
+                    })();
                 }
                 else {
                     webix.message({type: 'warning', text: 'Please, at first select any wordgroup for testing!'});
                 }
-            });
+                // });
+            }
+
+            $$('en_rus_test').attachEvent('onItemClick', (id) => { generateTestHandler(id); });
+            $$('rus_en_test').attachEvent('onItemClick', (id) => { generateTestHandler(id); });
         }
     };
 });
